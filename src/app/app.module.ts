82dc285b9fd772dbc0c858f7ef47ac3ea860b404
Nicolas Fajersztejn <Nicolas.Fajersztejn@sciops.esa.int>
2017-08-29 17:06:26 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LogbookTableComponent } from './logbook-table/logbook-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogbookTableComponent
   ],
   imports: [
     BrowserModule
